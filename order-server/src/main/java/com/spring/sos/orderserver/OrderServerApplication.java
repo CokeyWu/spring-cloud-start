436fdbdb0891846814293a66521339ebe25b4d39
wuxinhai <543071977@qq.com>
2019-01-16 01:21:37 +0800

    init project order service

---
+++ order-server/src/main/java/com/spring/sos/orderserver/OrderServerApplication.java
@@ -0,0 +1,14 @@
+package com.spring.sos.orderserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderServerApplication.class, args);
+    }
+
+}
+
